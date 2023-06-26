ef14e4eb9d977fb1b2f9073a7dd89af97d6125d2
adriannavenice <115912375+adriannavenice@users.noreply.github.com>
2023-06-26 18:59:24 -0400

    weighted graph

---
+++ src/graph/weightedGraph.py
@@ -0,0 +1,97 @@
+class WeightedEdge:
+    def __init__(self, initNode1, initNode2, initWeight):
+        self.node1 = initNode1
+        self.node2 = initNode2
+        self.weight = initWeight
+
+    def getNode1(self):
+        return self.node1
+
+    def getNode2(self):
+        return self.node2
+
+    def getWeight(self):
+        return self.weight
+
+
+class WeightedGraph:
+    def __init__(self):
+        self.nodes = {}
+        self.edges = {}
+
+    def get_keys(self):
+        return list(self.nodes.keys())
+
+    def node_exists(self, test_node):
+        return test_node in self.nodes
+
+    def get_edge_id(self, node1, node2):
+        return f"{node1}-{node2}"
+
+    def add_node(self, node_id, node_data):
+        self.nodes[node_id] = node_data
+
+    def get_node_data(self, node_id):
+        return self.nodes.get(node_id)
+
+    def add_edge(self, node1, node2, weight):
+        edge_id = self.get_edge_id(node1, node2)
+        self.edges[edge_id] = WeightedEdge(node1, node2, weight)
+
+    def remove_edge(self, node1, node2):
+        edge_id = self.get_edge_id(node1, node2)
+        if edge_id in self.edges:
+            del self.edges[edge_id]
+
+    def get_neighbors(self, node):
+        neighbors = []
+        for edge_id, edge in self.edges.items():
+            start_node, end_node = edge_id.split("-")
+            if start_node == node:
+                neighbors.append(end_node)
+        return neighbors
+
+    def are_neighbors(self, node1, node2):
+        neighbors = self.get_neighbors(node1)
+        return node2 in neighbors
+
+    def get_neighbor_weight(self, node1, node2):
+        if self.are_neighbors(node1, node2):
+            edge_id = self.get_edge_id(node1, node2)
+            return self.edges[edge_id].get_weight()
+        return 0.0
+
+    def find_path(self, node1, node2):
+        path = []
+        if not self.node_exists(node1) or not self.node_exists(node2):
+            return path
+
+        path.append(node1)
+        visited = {node1: node1}
+
+        while len(path) > 0:
+            last = path[-1]
+            neighbors = self.get_neighbors(last)
+            closest_index = -1
+            closest_distance = 100000.0
+
+            for i, neighbor in enumerate(neighbors):
+                if neighbor == node2:
+                    path.append(neighbor)
+                    return path
+
+                if neighbor not in visited:
+                    edge_id = self.get_edge_id(last, neighbor)
+                    edge = self.edges[edge_id]
+                    if edge.get_weight() < closest_distance:
+                        closest_index = i
+                        closest_distance = edge.get_weight()
+
+            if closest_index >= 0:
+                closest_node = neighbors[closest_index]
+                visited[closest_node] = closest_node
+                path.append(closest_node)
+            elif len(path) > 0:
+                path.pop()
+
+        return path
---
+++ src/tps/pyTPS.py
@@ -0,0 +1,92 @@
+class pyTPS:
+    def __init__(self):
+        self.transactions = []
+        self.numTransactions = 0
+        self.mostRecentTransaction = -1
+        self.performingDo = False
+        self.performingUndo = False
+
+    def isPerformingDo(self):
+        return self.performingDo
+
+    def isPerformingUndo(self):
+        return self.performingUndo
+
+    def hasTransactionToRedo(self):
+        return (self.mostRecentTransaction + 1) < self.numTransactions
+
+    def hasTransactionToUndo(self):
+        return self.mostRecentTransaction >= 0
+
+    def addTransaction(self, transaction):
+        # ARE WE BRANCHING?
+        if (
+            self.mostRecentTransaction < 0
+            or self.mostRecentTransaction < (len(self.transactions) - 1)
+        ):
+            for i in range(len(self.transactions) - 1, self.mostRecentTransaction, -1):
+                del self.transactions[i]
+            self.numTransactions = self.mostRecentTransaction + 2
+        else:
+            self.numTransactions += 1
+
+        # ADD THE TRANSACTION
+        self.transactions.append(transaction)
+
+        # AND EXECUTE IT
+        self.doTransaction()
+
+    def doTransaction(self):
+        if self.hasTransactionToRedo():
+            self.performingDo = True
+            transaction = self.transactions[self.mostRecentTransaction + 1]
+            transaction.doTransaction()
+            self.mostRecentTransaction += 1
+            self.performingDo = False
+
+    def undoTransaction(self):
+        if self.hasTransactionToUndo():
+            self.performingUndo = True
+            transaction = self.transactions[self.mostRecentTransaction]
+            transaction.undoTransaction()
+            self.mostRecentTransaction -= 1
+            self.performingUndo = False
+
+    def clearAllTransactions(self):
+        self.transactions = []
+        self.mostRecentTransaction = -1
+        self.numTransactions = 0
+
+    def getSize(self):
+        return len(self.transactions)
+
+    def getRedoSize(self):
+        return self.getSize() - self.mostRecentTransaction - 1
+
+    def getUndoSize(self):
+        return self.mostRecentTransaction + 1
+
+    def toString(self):
+        text = "--Number of Transactions: {}\n".format(self.numTransactions)
+        text += "--Current Index on Stack: {}\n".format(self.mostRecentTransaction)
+        text += "--Current Transaction Stack:\n"
+        for i in range(self.mostRecentTransaction + 1):
+            transaction = self.transactions[i]
+            text += "----{}\n".format(transaction.toString())
+        return text
+
+from abc import ABC, abstractmethod
+
+class pyTPS_Transaction(ABC):
+    @abstractmethod
+    def doTransaction(self):
+        pass
+
+    @abstractmethod
+    def undoTransaction(self):
+        pass
+
+    @abstractmethod
+    def toString(self):
+        pass
+
